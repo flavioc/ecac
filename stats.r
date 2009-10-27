ad43c6b71d40a9f2f59f49c14a407b42bd901b12
Joao Azevedo <joao.c.azevedo@gmail.com>
2009-10-27 17:55:55 +0000

    added R script for data loading

---
+++ stats.r
@@ -0,0 +1,19 @@
+### loads the required libraries
+library(RMySQL)
+library(dprep)
+
+### connects and loads the table data
+con <- dbConnect(dbDriver("MySQL"), dbname="adult", user="root", password="root")
+data <- dbReadTable(con, "adult")
+
+### replaces unknown with empty values for missing values processing
+data[data == "unknown"] <- NA
+
+### turn strings into factors
+data$workclass <- factor(data$workclass)
+data$marital_status <- factor(data$marital_status)
+data$occupation <- factor(data$occupation)
+data$relationship <- factor(data$relationship)
+data$race <- factor(data$race)
+data$sex <- factor(data$sex)
+data$native_country <- factor(data$native_country)
